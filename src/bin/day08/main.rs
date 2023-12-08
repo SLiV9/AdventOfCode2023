a027cd2b6d43a972459f319a4a4ae1b8632325e7
Sander in 't Veld <sveld@block-tech.io>
2023-12-08 21:22:29 +0100

    Day 8, part two, first attempt at brute forcing won't work.

--- src/bin/day08/main.rs
+++ src/bin/day08/main.rs
@@ -36,7 +36,9 @@ const NUM_NAMES: usize = 26 * 26 * 26;
 
 fn encode_name(name: &[u8]) -> u16
 {
-	name.iter().fold(0, |x, c| x * 26 + u16::from(c - b'A'))
+	name.iter()
+		.rev()
+		.fold(0, |x, c| x * 26 + u16::from(c - b'A'))
 }
 
 #[derive(Debug)]
@@ -55,6 +57,11 @@ impl Graph
 			lefts: [0; NUM_NAMES],
 			rights: [0; NUM_NAMES],
 		};
+		for i in 0..MAX_NUM_GHOSTS
+		{
+			graph.lefts[i] = i as u16;
+			graph.rights[i] = i as u16;
+		}
 		for line in lines
 		{
 			let line = line.as_bytes();
@@ -69,9 +76,64 @@ impl Graph
 	}
 }
 
-fn two(input: &str) -> i32
+const MAX_NUM_GHOSTS: usize = 26 * 26;
+
+fn two(input: &str) -> usize
 {
-	input.len() as i32 * 0
+	let (instructions, rest) = input.split_once('\n').unwrap();
+	let graph = Graph::from_input(rest);
+	let mut instructions = instructions.as_bytes().iter().cycle();
+	let mut num_steps_taken = 0;
+
+	let first_end = (NUM_NAMES - MAX_NUM_GHOSTS) as u16;
+	let is_end = |x: u16| x >= first_end;
+
+	dbg!(graph.lefts.iter().filter(|&&x| is_end(x)).count());
+	dbg!(graph.rights.iter().filter(|&&x| is_end(x)).count());
+
+	let mut ixs = [0u16; MAX_NUM_GHOSTS];
+	let num_ghosts = {
+		let mut num_ghosts = 0;
+		for i in 0..MAX_NUM_GHOSTS
+		{
+			if graph.lefts[i] != i as u16 || graph.rights[i] != i as u16
+			{
+				ixs[num_ghosts] = i as u16;
+				num_ghosts += 1;
+			}
+		}
+		num_ghosts
+	};
+
+	for _ in 0..u64::from(u32::MAX)
+	{
+		if ixs[0..num_ghosts].iter().cloned().all(is_end)
+		{
+			break;
+		}
+
+		if instructions.next().unwrap() == &b'L'
+		{
+			for i in 0..num_ghosts
+			{
+				ixs[i] = graph.lefts[ixs[i] as usize];
+			}
+		}
+		else
+		{
+			for i in 0..num_ghosts
+			{
+				ixs[i] = graph.rights[ixs[i] as usize];
+			}
+		}
+		num_steps_taken += 1;
+
+		if num_steps_taken % usize::from(u16::MAX) == 0
+		{
+			dbg!(num_steps_taken);
+		}
+	}
+	num_steps_taken
 }
 
 #[cfg(test)]
@@ -83,6 +145,7 @@ mod tests
 	const PROVIDED: &str = include_str!("provided.txt");
 	const PROVIDED_SHUFFLED: &str = include_str!("provided_shuffled.txt");
 	const PROVIDED2: &str = include_str!("provided2.txt");
+	const PROVIDED_TWO: &str = include_str!("provided_two.txt");
 
 	#[test]
 	fn sanity()
@@ -95,6 +158,14 @@ mod tests
 	{
 		assert_eq!(encode_name("AAA".as_bytes()), 0);
 		assert_eq!(usize::from(encode_name("ZZZ".as_bytes())), NUM_NAMES - 1);
+		assert_eq!(
+			usize::from(encode_name("ZZA".as_bytes())),
+			MAX_NUM_GHOSTS - 1
+		);
+		assert_eq!(
+			usize::from(encode_name("AAZ".as_bytes())),
+			NUM_NAMES - MAX_NUM_GHOSTS
+		);
 	}
 
 	#[test]
@@ -116,8 +187,8 @@ mod tests
 	}
 
 	#[test]
-	fn two_provided()
+	fn two_provided_two()
 	{
-		assert_eq!(two(PROVIDED), 0);
+		assert_eq!(two(PROVIDED_TWO), 6);
 	}
 }
