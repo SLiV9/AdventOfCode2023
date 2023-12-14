ab410e0215052e329d35b46a2b9721b713291642
Sander in 't Veld <sveld@block-tech.io>
2023-12-14 19:50:21 +0100

    Day 14, part two, first attempt is too high.

--- src/bin/day14/main.rs
+++ src/bin/day14/main.rs
@@ -1,6 +1,7 @@
 /**/
 
 use aoc2023::run;
+use smallvec::SmallVec;
 
 const INPUT: &str = include_str!("input.txt");
 
@@ -44,9 +45,255 @@ fn one(input: &str) -> usize
 	total_load
 }
 
-fn two(input: &str) -> i32
+#[derive(Debug)]
+struct Grid
 {
-	input.len() as i32 * 0
+	data: [u128; 128],
+}
+
+impl Grid
+{
+	fn empty() -> Grid
+	{
+		Grid { data: [0u128; 128] }
+	}
+
+	fn set(&mut self, r: usize, c: usize)
+	{
+		self.data[r] |= 1 << c;
+	}
+
+	fn clear(&mut self, r: usize, c: usize)
+	{
+		self.data[r] &= !(1 << c);
+	}
+
+	fn get(&self, r: usize, c: usize) -> bool
+	{
+		(self.data[r] & (1 << c)) != 0
+	}
+
+	fn roll_north(&mut self, pillars: &Grid, num_rows: usize, num_cols: usize)
+	{
+		let mut roll_point = [0usize; 128];
+		for r in 0..num_rows
+		{
+			for c in 0..num_cols
+			{
+				if pillars.get(r, c)
+				{
+					roll_point[c] = r + 1;
+				}
+				else if self.get(r, c)
+				{
+					if roll_point[c] < r
+					{
+						self.clear(r, c);
+						self.set(roll_point[c], c);
+						roll_point[c] += 1;
+					}
+					else
+					{
+						roll_point[c] = r + 1;
+					}
+				}
+			}
+		}
+	}
+
+	fn roll_south(&mut self, pillars: &Grid, num_rows: usize, num_cols: usize)
+	{
+		let mut roll_point = [num_rows - 1; 128];
+		for r in (0..num_rows).rev()
+		{
+			for c in 0..num_cols
+			{
+				if pillars.get(r, c)
+				{
+					roll_point[c] = r.saturating_sub(1);
+				}
+				else if self.get(r, c)
+				{
+					if roll_point[c] > r
+					{
+						self.clear(r, c);
+						self.set(roll_point[c], c);
+						roll_point[c] -= 1;
+					}
+					else
+					{
+						roll_point[c] = r.saturating_sub(1)
+					}
+				}
+			}
+		}
+	}
+
+	fn roll_west(&mut self, pillars: &Grid, num_rows: usize, num_cols: usize)
+	{
+		let mut roll_point = [0usize; 128];
+		for c in 0..num_cols
+		{
+			for r in 0..num_rows
+			{
+				if pillars.get(r, c)
+				{
+					roll_point[r] = c + 1;
+				}
+				else if self.get(r, c)
+				{
+					if roll_point[r] < c
+					{
+						self.clear(r, c);
+						self.set(r, roll_point[r]);
+						roll_point[r] += 1;
+					}
+					else
+					{
+						roll_point[r] = c + 1;
+					}
+				}
+			}
+		}
+	}
+
+	fn roll_east(&mut self, pillars: &Grid, num_rows: usize, num_cols: usize)
+	{
+		let mut roll_point = [num_cols - 1; 128];
+		for c in (0..num_cols).rev()
+		{
+			for r in 0..num_rows
+			{
+				if pillars.get(r, c)
+				{
+					roll_point[r] = c.saturating_sub(1);
+				}
+				else if self.get(r, c)
+				{
+					if roll_point[r] > c
+					{
+						self.clear(r, c);
+						self.set(r, roll_point[r]);
+						roll_point[r] -= 1;
+					}
+					else
+					{
+						roll_point[r] = c.saturating_sub(1)
+					}
+				}
+			}
+		}
+	}
+
+	fn load_on_north_pillar(&self, num_rows: usize) -> u32
+	{
+		let mut num_boulders = 0;
+		let mut total_load = 0;
+		for row in &self.data[0..num_rows]
+		{
+			num_boulders += row.count_ones();
+			total_load += num_boulders;
+		}
+		total_load
+	}
+}
+
+const NUM_CYCLES: usize = 1000000000;
+const NUM_ITERATIONS: usize = 4 * NUM_CYCLES;
+
+fn two(input: &str) -> u32
+{
+	run_simulation(input, NUM_ITERATIONS)
+}
+
+fn run_simulation(input: &str, num_iterations: usize) -> u32
+{
+	let mut pillar_grid = Grid::empty();
+	let mut boulder_grid = Grid::empty();
+	let mut num_rows = 0;
+	let mut num_cols = 0;
+	for (r, line) in input.lines().filter(|x| !x.is_empty()).enumerate()
+	{
+		for (c, x) in line.as_bytes().iter().enumerate()
+		{
+			match *x
+			{
+				b'#' => pillar_grid.set(r, c),
+				b'O' => boulder_grid.set(r, c),
+				b'.' => (),
+				_ => unreachable!(),
+			}
+		}
+		num_rows = r + 1;
+		num_cols = line.as_bytes().len();
+	}
+
+	let mut historic_loads: SmallVec<[u32; 1024]> = SmallVec::new();
+	for t in 0..num_iterations
+	{
+		match t % 4
+		{
+			0 => boulder_grid.roll_north(&pillar_grid, num_rows, num_cols),
+			1 => boulder_grid.roll_west(&pillar_grid, num_rows, num_cols),
+			2 => boulder_grid.roll_south(&pillar_grid, num_rows, num_cols),
+			3 => boulder_grid.roll_east(&pillar_grid, num_rows, num_cols),
+			_ => unreachable!(),
+		}
+
+		if cfg!(test)
+		{
+			for r in 0..num_rows
+			{
+				for c in 0..num_cols
+				{
+					if pillar_grid.get(r, c)
+					{
+						print!("#");
+					}
+					else if boulder_grid.get(r, c)
+					{
+						print!("O");
+					}
+					else
+					{
+						print!(".");
+					}
+				}
+				println!();
+			}
+		}
+
+		if t % 4 == 3
+		{
+			// dbg!(t);
+			let load = boulder_grid.load_on_north_pillar(num_rows);
+			// dbg!(load);
+			historic_loads.push(load);
+
+			if t > 400
+			{
+				for k in 1..80
+				{
+					if (num_iterations - (t + 1)) % k != 0
+					{
+						continue;
+					}
+					let i = historic_loads.len() - 1;
+					if (1..5)
+						.map(|j| historic_loads[i - k * j])
+						.all(|x| x == load)
+					{
+						return load;
+					}
+				}
+			}
+		}
+	}
+	match historic_loads.last()
+	{
+		Some(final_load) => *final_load,
+		None => boulder_grid.load_on_north_pillar(num_rows),
+	}
 }
 
 #[cfg(test)]
@@ -64,6 +311,18 @@ mod tests
 	}
 
 	#[test]
+	fn one_simulation_provided()
+	{
+		assert_eq!(run_simulation(PROVIDED, 1), 136);
+	}
+
+	#[test]
+	fn one_simulation_12_provided()
+	{
+		run_simulation(PROVIDED, 12);
+	}
+
+	#[test]
 	fn test_boulder_drop()
 	{
 		assert_eq!(one(".\n.\n.\nO"), 4);
@@ -74,6 +333,12 @@ mod tests
 	#[test]
 	fn two_provided()
 	{
-		assert_eq!(two(PROVIDED), 0);
+		assert_eq!(two(PROVIDED), 64);
+	}
+
+	#[test]
+	fn two_simulation_provided()
+	{
+		run_simulation(PROVIDED, 1000);
 	}
 }
