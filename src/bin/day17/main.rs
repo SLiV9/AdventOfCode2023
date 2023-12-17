56d700e73b1fc3a0311cf06ea86d29f0109cb751
Sander in 't Veld <sveld@block-tech.io>
2023-12-17 20:08:59 +0100

    Day 17, part two using Dijkstra in 500+ ms.

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -1,12 +1,12 @@
 //!
 
 use aoc2023::run;
-use std::cmp::Reverse;
-use std::collections::{BinaryHeap, HashMap};
 
 const INPUT: &str = include_str!("input.txt");
 
 const GRID_SIZE: usize = 192;
+
+const MAX_COST: usize = 9;
 
 pub fn main()
 {
@@ -19,20 +19,7 @@ fn one(input: &str) -> usize
 	let mut cost_grid = [[0; GRID_SIZE]; GRID_SIZE];
 	let (num_rows, num_cols) = parse_grid(&mut cost_grid, input);
 
-	let start = Explorer::default();
-	let target = Point {
-		row: (num_rows - 1) as u8,
-		col: (num_cols - 1) as u8,
-	};
-
-	find_least_cost_with_a_star(
-		&cost_grid,
-		num_rows,
-		num_cols,
-		start,
-		target,
-		CrucibleModel::Regular,
-	)
+	find_least_cost(&cost_grid, num_rows, num_cols, CrucibleModel::Regular)
 }
 
 fn two(input: &str) -> usize
@@ -40,20 +27,7 @@ fn two(input: &str) -> usize
 	let mut cost_grid = [[0; GRID_SIZE]; GRID_SIZE];
 	let (num_rows, num_cols) = parse_grid(&mut cost_grid, input);
 
-	let start = Explorer::default();
-	let target = Point {
-		row: (num_rows - 1) as u8,
-		col: (num_cols - 1) as u8,
-	};
-
-	find_least_cost_with_a_star(
-		&cost_grid,
-		num_rows,
-		num_cols,
-		start,
-		target,
-		CrucibleModel::Ultra,
-	)
+	find_least_cost(&cost_grid, num_rows, num_cols, CrucibleModel::Ultra)
 }
 
 fn parse_grid(
@@ -85,6 +59,13 @@ struct Point
 
 impl Point
 {
+	fn get<T: Copy>(self, grid: &[[T; GRID_SIZE]; GRID_SIZE]) -> T
+	{
+		let r = self.row as usize;
+		let c = self.col as usize;
+		grid[r][c]
+	}
+
 	fn step(
 		self,
 		direction: Direction,
@@ -131,6 +112,8 @@ enum Direction
 	North,
 }
 
+const DIRECTION_LEN: usize = 4;
+
 impl Direction
 {
 	fn turn_left(self) -> Direction
@@ -161,7 +144,32 @@ struct Explorer
 {
 	at: Point,
 	facing: Direction,
-	strain: u8,
+}
+
+impl Explorer
+{
+	fn get<T: Copy>(
+		self,
+		grid: &[[[T; DIRECTION_LEN]; GRID_SIZE]; GRID_SIZE],
+	) -> T
+	{
+		let r = self.at.row as usize;
+		let c = self.at.col as usize;
+		let d = self.facing as usize;
+		grid[r][c][d]
+	}
+
+	fn set<T: Copy>(
+		self,
+		grid: &mut [[[T; DIRECTION_LEN]; GRID_SIZE]; GRID_SIZE],
+		value: T,
+	)
+	{
+		let r = self.at.row as usize;
+		let c = self.at.col as usize;
+		let d = self.facing as usize;
+		grid[r][c][d] = value;
+	}
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -173,7 +181,7 @@ enum CrucibleModel
 
 impl CrucibleModel
 {
-	fn max_strain(self) -> u8
+	fn max_strain(self) -> usize
 	{
 		match self
 		{
@@ -182,7 +190,7 @@ impl CrucibleModel
 		}
 	}
 
-	fn min_strain(self) -> u8
+	fn min_strain(self) -> usize
 	{
 		match self
 		{
@@ -192,136 +200,145 @@ impl CrucibleModel
 	}
 }
 
-impl Explorer
-{
-	fn step_forward(
-		self,
-		num_rows: usize,
-		num_cols: usize,
-		model: CrucibleModel,
-	) -> Option<Explorer>
-	{
-		if self.strain == model.max_strain()
-		{
-			return None;
-		}
-		let at = self.at.step(self.facing, num_rows, num_cols)?;
-		Some(Explorer {
-			at,
-			facing: self.facing,
-			strain: self.strain + 1,
-		})
-	}
-
-	fn turn_left(
-		self,
-		num_rows: usize,
-		num_cols: usize,
-		model: CrucibleModel,
-	) -> Option<Explorer>
-	{
-		if self.strain < model.min_strain()
-		{
-			return None;
-		}
-		let facing = self.facing.turn_left();
-		let at = self.at.step(facing, num_rows, num_cols)?;
-		Some(Explorer {
-			at,
-			facing,
-			strain: 0,
-		})
-	}
-
-	fn turn_right(
-		self,
-		num_rows: usize,
-		num_cols: usize,
-		model: CrucibleModel,
-	) -> Option<Explorer>
-	{
-		if self.strain < model.min_strain()
-		{
-			return None;
-		}
-		let facing = self.facing.turn_right();
-		let at = self.at.step(facing, num_rows, num_cols)?;
-		Some(Explorer {
-			at,
-			facing,
-			strain: 0,
-		})
-	}
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
 struct Candidate
 {
-	f_score: usize,
+	rank: usize,
 	explorer: Explorer,
 }
 
-fn find_least_cost_with_a_star(
+const MAX_INITIAL_SHORTLIST_LEN: usize = 16;
+
+fn find_least_cost(
 	cost_grid: &[[u8; GRID_SIZE]; GRID_SIZE],
 	num_rows: usize,
 	num_cols: usize,
-	start: Explorer,
-	target: Point,
 	model: CrucibleModel,
 ) -> usize
 {
+	let start = Point { row: 0, col: 0 };
+	let target = Point {
+		row: (num_rows - 1) as u8,
+		col: (num_cols - 1) as u8,
+	};
 	let manhattan_distance = |from: Point, to: Point| {
 		let dr = (to.row as i32 - from.row as i32).abs() as usize;
 		let dc = (to.col as i32 - from.col as i32).abs() as usize;
 		dr + dc
 	};
-	let heuristic = |x: Explorer| manhattan_distance(x.at, target);
-	let get_cost = |x: Explorer| {
-		let r = x.at.row as usize;
-		let c = x.at.col as usize;
-		cost_grid[r][c]
-	};
-	// let mut upper_bound = manhattan_distance(start.at, target) * MAX_COST;
+	let upper_bound = manhattan_distance(start, target) * MAX_COST;
+	let mut dist = [[[upper_bound; DIRECTION_LEN]; GRID_SIZE]; GRID_SIZE];
+	let mut buffer = [Candidate::default(); GRID_SIZE * MAX_COST * 10];
+	let mut shortlist_start = 0;
+	let mut shortlist_end = 0;
+	let mut shortlist_rank_threshold = 0;
+	let mut len = 0;
 
-	let mut open_set = BinaryHeap::new();
-	let mut g_score_map = HashMap::new();
-	g_score_map.insert(start, 0);
-	open_set.push(Reverse(Candidate {
-		explorer: start,
-		f_score: heuristic(start),
-	}));
-
-	while let Some(candidate) = open_set.pop()
+	for facing in [Direction::East, Direction::South]
 	{
-		let Reverse(Candidate {
-			explorer,
-			f_score: _,
-		}) = candidate;
-		let g_score = g_score_map[&explorer];
-		let neighbors = [
-			explorer.step_forward(num_rows, num_cols, model),
-			explorer.turn_left(num_rows, num_cols, model),
-			explorer.turn_right(num_rows, num_cols, model),
-		];
-		for next in neighbors.into_iter().filter_map(|x| x)
+		let explorer = Explorer { at: start, facing };
+		explorer.set(&mut dist, 0);
+		buffer[len] = Candidate { explorer, rank: 0 };
+		len += 1;
+	}
+
+	while len > 0
+	{
+		debug_assert!(shortlist_start <= shortlist_end);
+		debug_assert!(shortlist_end <= len);
+		if shortlist_start == shortlist_end
 		{
-			let cost = get_cost(next) as usize;
-			let g_score = g_score + cost;
-			if next.at == target && next.strain >= model.min_strain()
+			buffer[0..len].rotate_left(shortlist_end);
+			len -= shortlist_end;
+			if len == 0
 			{
-				return g_score;
+				break;
 			}
-			if g_score < g_score_map.get(&next).map_or(usize::MAX, |x| *x)
+			buffer[0..len].sort_unstable();
+			shortlist_start = 0;
+			shortlist_end = len.min(MAX_INITIAL_SHORTLIST_LEN);
+			shortlist_rank_threshold = buffer[shortlist_end - 1].rank;
+		}
+
+		let Candidate {
+			explorer: curr,
+			rank: current_dist,
+		} = buffer[shortlist_start];
+		shortlist_start += 1;
+
+		for facing in [curr.facing.turn_left(), curr.facing.turn_right()]
+		{
+			let mut next = Explorer {
+				at: curr.at,
+				facing,
+			};
+			let mut cost = current_dist;
+			for strain in 0..=model.max_strain()
 			{
-				g_score_map.insert(next, g_score);
-				open_set.push(Reverse(Candidate {
-					explorer: next,
-					f_score: g_score + heuristic(next),
-				}));
+				let Some(at) = next.at.step(facing, num_rows, num_cols)
+				else
+				{
+					break;
+				};
+				next.at = at;
+				cost += at.get(cost_grid) as usize;
+
+				if strain < model.min_strain()
+				{
+					continue;
+				}
+
+				if cost < next.get(&dist)
+				{
+					next.set(&mut dist, cost);
+
+					let rank = cost;
+					buffer[len] = Candidate {
+						explorer: next,
+						rank,
+					};
+					if rank < shortlist_rank_threshold
+					{
+						buffer.swap(shortlist_end, len);
+						let i = buffer[shortlist_start..shortlist_end]
+							.partition_point(|&c| c.rank < rank);
+						shortlist_end += 1;
+						buffer[i..shortlist_end].rotate_right(1);
+					}
+					len += 1;
+				}
 			}
 		}
 	}
-	unreachable!()
+
+	dist[target.row as usize][target.col as usize]
+		.into_iter()
+		.min()
+		.unwrap()
+}
+
+#[allow(unused)]
+#[cfg(debug_assertions)]
+fn print_dist(
+	grid: &[[[usize; DIRECTION_LEN]; GRID_SIZE]; GRID_SIZE],
+	num_rows: usize,
+	num_cols: usize,
+)
+{
+	println!();
+	for row in &grid[0..num_rows]
+	{
+		for cell in &row[0..num_cols]
+		{
+			print!("E{:04}", cell[0]);
+			print!("S{:04}", cell[1]);
+			print!("W{:04}", cell[2]);
+			print!("N{:04}", cell[3]);
+			print!(" ");
+		}
+		println!();
+	}
+	println!();
 }
 
 #[cfg(test)]
@@ -340,9 +357,21 @@ mod tests
 	}
 
 	#[test]
+	fn one_horizontal()
+	{
+		assert_eq!(one("0123"), 6);
+	}
+
+	#[test]
+	fn one_vertical()
+	{
+		assert_eq!(one("07\n18\n19\n11"), 4);
+	}
+
+	#[test]
 	fn size_of_structs()
 	{
-		assert_eq!(std::mem::size_of::<Explorer>(), 4);
+		assert!(std::mem::size_of::<Explorer>() <= 4);
 	}
 
 	#[test]
